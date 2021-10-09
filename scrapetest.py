718648f9332f2787910466d5a157d0868ac20bf1
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-10-09 00:10:53 -0300

    Handling with possible URLError too

--- scrapetest.py
+++ scrapetest.py
@@ -1,6 +1,18 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
 
-html = urlopen('http://www.pythonscraping.com/pages/page1.html')
-bs = BeautifulSoup(html, 'html.parser')
-print(bs.html)
+
+try:
+    html = urlopen('http://www.pythonscraping.com/pages/page1.html')
+except HTTPError as e:
+    print(e)
+except URLError as e:
+    print('The server could not be found!')
+    # devolve null, executa um break ou algum outro "Plano B"
+else:
+    # o programa continua. Nota: se você retornar ou executar um break no
+    # catch da exceção, não será necessário usar a instrução "else"
+    bs = BeautifulSoup(html, 'html.parser')
+    print(bs.html)
+    print('It worked!')
