ab0dc53e5ce7b9bf02aa5053c160b1828734f76b
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-10-09 00:08:50 -0300

    Handling with possible HTTPError

--- scrapetest.py
+++ scrapetest.py
@@ -2,5 +2,5 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 html = urlopen('http://www.pythonscraping.com/pages/page1.html')
-bs = BeautifulSoup(html.read(), 'html.parser')
-print(bs.h1)
+bs = BeautifulSoup(html, 'html.parser')
+print(bs.html)
